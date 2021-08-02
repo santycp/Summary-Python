2a8f97332644211edb8f501321fa10f21fef46dc
santy <santy.calvo,p>
2021-08-02 15:33:58 -0500

    add note

--- Basic Conceps.py
+++ Basic Conceps.py
@@ -8,3 +8,6 @@ print( 100 -1 + 3)
 #use parentheses to determinate wich operations are performed first
 
 print(2*(3+4))
+
+
+# NOTE: Using a single slash to divide numbers produces a decimal (or float, as it is called in programming). We'll have more about floats in the next lesson.
