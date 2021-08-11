df2f8b2ff629bc92c931bb87d6ee356dd5600fa8
santy <santy.calvo,p>
2021-08-11 14:54:46 -0500

    More Types

--- More Types.py
+++ More Types.py
@@ -142,3 +142,71 @@ print(msg) # Numbers: 4 5 6
 #String formatting can also be done with named arguments.
 a = "{x}, {y}".format(x=5, y=12)
 print(a) #5, 12
+
+#------ STRING FUNCTIONS ------
+'''
+join - joins a list of strings with another string as a separator.
+replace - replaces one substring in a string with another.
+startswith and endswith - determine if there is a substring at the start and end of a string, respectively.
+To change the case of a string, you can use lower and upper.
+The method split is the opposite of join turning a string with a certain separator into a list.
+'''
+
+print(", ".join(["spam", "eggs", "ham"]))
+#prints "spam, eggs, ham"
+
+print("Hello ME".replace("ME", "world"))
+#prints "Hello world"
+
+print("This is a sentence.".startswith("This"))
+# prints "True"
+
+print("This is a sentence.".endswith("sentence."))
+# prints "True"
+
+print("This is a sentence.".upper())
+# prints "THIS IS A SENTENCE."
+
+print("AN ALL CAPS SENTENCE".lower())
+#prints "an all caps sentence"
+
+print("spam, eggs, ham".split(", "))
+#prints "['spam', 'eggs', 'ham']"
+        
+#----- Numeric Funcitons -------
+
+'''
+To find the maximum or minimum of some numbers or a list, you can use max or min.
+To find the distance of a number from zero (its absolute value), use abs.
+To round a number to a certain number of decimal places, use round.
+To find the total of a list, use sum.
+'''
+
+print(min(1, 2, 3, 4, 0, 2, 1)) # 0
+print(max([1, 4, 9, 2, 5, 6, 8])) #9
+print(abs(-99)) #99
+print(abs(42)) #44
+print(sum([1, 2, 3, 4, 5])) #15
+
+#------ List Functions -------
+'''
+Often used in conditional statements, all and any take a list as an argument, and return True if all or any 
+(respectively)  of their arguments evaluate to True (and False otherwise).
+The function enumerate can be used to iterate through the values and indices of a list simultaneously.
+'''
+
+nums = [55, 44, 33, 22, 11]
+
+if all([i > 5 for i in nums]):
+    print("All larger than 5") #All larger than 5
+
+if any([i % 2 == 0 for i in nums]):
+    print("At least one is even") #At least one is even
+
+for v in enumerate(nums):
+    print(v) #(0, 55) (1, 44) (2, 33) (3, 22) (4, 11)
+
+
+
+
+
