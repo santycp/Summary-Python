8e1f2be1566f4891e063085e2870d69ff9ee4460
santy <santy.calvo,p>
2021-09-01 22:28:59 -0500

    Regularexpresions

--- RegularExpresion.py
+++ RegularExpresion.py
@@ -338,3 +338,71 @@ if match:
     print("Match 3")
 
 
+#------ ESPECIAL SEQUENCES ------
+
+#They are written as a backslash followed by another character.
+
+import re
+
+pattern = r"(.+) \1"
+
+match = re.match(pattern, "word word")
+if match:
+    print ("Match 1") #result
+
+match = re.match(pattern, "?! ?!")
+if match:
+    print ("Match 2") #result
+
+match = re.match(pattern, "abc cde")
+if match:
+    print ("Match 3")
+
+'''
+More useful special sequences are \d, \s, and \w.
+These match digits, whitespace, and word characters respectively.
+In ASCII mode they are equivalent to [0-9], [ \t\n\r\f\v], and [a-zA-Z0-9_].
+In Unicode mode they match certain other characters, as well. For instance, \w matches letters with accents.
+Versions of these special sequences with upper case letters - \D, \S, and \W - mean the opposite to the lower-case versions. For instance, \D matches anything that isn't a digit. 
+'''
+
+
+import re
+
+pattern = r"(\D+\d)"
+
+match = re.match(pattern, "Hi 999!")
+if match:
+    print("Match 1") #result
+
+match = re.match(pattern, "1, 23, 456!")
+if match:
+    print("Match 2")
+
+match = re.match(pattern, " ! $?")
+if match:
+    print("Match 3")
+
+
+"""
+Additional special sequences are \A, \Z, and \b.
+The sequences \A and \Z match the beginning and end of a string, respectively.
+The sequence \b matches the empty string between \w and \W characters, or \w characters and the beginning or end of the string. Informally, it represents the boundary between words.
+The sequence \B matches the empty string anywhere else. 
+"""
+
+import re
+
+pattern = r"\b(cat)\b"
+
+match = re.search(pattern, "The cat sat!")
+if match:
+    print ("Match 1") #result
+
+match = re.search(pattern, "We s>cat<tered?")
+if match:
+    print ("Match 2") #result
+
+match = re.search(pattern, "We scattered.")
+if match:
+    print ("Match 3")
